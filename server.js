0c7e154e8d379e4589213c523bb27a6c6a5511a8
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2023-10-18 22:27:43 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+
+//routes
+app.get('/',(req,res)=>{
+    res.send(' node api');
+})
+
+
+app.listen(3000,()=>{
+    console.log('node api is running here')
+})
