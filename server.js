f425f39e43768cd855dd81bc3de9216cf2f59069
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2023-10-20 22:42:10 +0530

    connection with MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 
 
@@ -9,7 +9,16 @@ app.get('/',(req,res)=>{
     res.send(' hello api');
 })
 
-
-app.listen(3000,()=>{
-    console.log('node api is running here')
-})
+// connnection with db 
+mongoose
+  .connect(
+    "mongodb+srv://nandunsb99:Nandana99@cluster0.vprjlwa.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to mongodb")
+    app.listen(3000, () => {
+      console.log("node api is running here")
+    })
+  }) .catch((error) => {
+    console.log(error);
+  })
