627748bb5bf5d59f3a36be437b0c6826c3ce01b0
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2023-10-22 00:15:53 +0530

    Fetching all products from database

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.get('/',(req,res)=>{
     res.send(' hai api');
 })
 
-app.post('/product', async(req,res)=>{
+//store the data in db
+
+app.post('/products', async(req,res)=>{
    try{
     const product = await Product.create(req.body)
     res.status(200).json(product);
@@ -22,6 +24,20 @@ app.post('/product', async(req,res)=>{
     res.status(500).json({message: error.message})
    }
 })
+
+//Fetch all products from db
+app.get('/products',async(req,res)=>{
+    try{
+        const products = await Product.find({});
+        res.status(200).json({products})
+
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+
+})
+
+
 
 // connnection with db 
 mongoose
